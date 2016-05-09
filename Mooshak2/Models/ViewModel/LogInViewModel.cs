68d8d2d8cde70473df6ae2e41bbdafbea52d5c3b
eirikura <ekkertlaust@gmail.com>
2016-05-09 17:30:00 +0000

    Bætti við LogInView og AdminView

---
+++ Mooshak2/Models/ViewModel/LogInViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.ViewModel
+{
+    public class LogInViewModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string username { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public String password { get; set; }
+    }
+}
