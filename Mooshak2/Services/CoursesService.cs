d65be60f8cf36f0e443144b3b51586778a193616
Maciuska <Maciuska14@gmail.com>
2016-05-09 23:04:16 +0000

    Added summary to CoursesService functions

--- Mooshak2/Services/CoursesService.cs
+++ Mooshak2/Services/CoursesService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mooshak2.Models.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,23 +7,25 @@ using System.Web;
 namespace Mooshak2.Services
 {
     /// <summary>
-    /// 
+    /// This service is mainly used to connect entities to the database and retrive data
+    /// by a specified value, for example: getCourseNameByCourseID returns a course
+    /// name by finding a given course ID assigned to it.
     /// </summary>
     public class CoursesService
     {
         /// <summary>
-        /// 
+        /// This functions returns a courses name assigned to the given course ID.
         /// </summary>
         /// <param name="courseID"></param>
         /// <returns></returns>
-        public CourseCreateEditViewModel getCourseNameByID (int courseID)
+        public CourseCreateEditViewModel getCourseNameByCourseID (int courseID)
         {
             //TODO:
             return null;
         }
 
         /// <summary>
-        /// 
+        /// This function returns the courses description assigned to the given course ID.
         /// </summary>
         /// <param name="courseID"></param>
         /// <returns></returns>
