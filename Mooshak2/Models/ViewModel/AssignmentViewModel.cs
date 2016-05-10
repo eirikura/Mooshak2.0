7fbddaf596bce308fb0c2f0e311fa59556c882f2
Maciuska <Maciuska14@gmail.com>
2016-05-10 13:34:05 +0000

    AssignmentViewModel summaries filled

--- Mooshak2/Models/ViewModel/AssignmentViewModel.cs
+++ Mooshak2/Models/ViewModel/AssignmentViewModel.cs
@@ -6,32 +6,34 @@ using System.Web;
 namespace Mooshak2.Models.ViewModel
 {
     /// <summary>
-    /// This view will 
+    /// The assignment view model is responsible for showing current assignments. It is used by students
+    /// in order to submit an assignemnt or a part of it and see details of an assignment like
+    /// for example, due date of an assignment.
     /// </summary>
     public class AssignmentViewModel
     {
         /// <summary>
-        /// 
+        /// The name of the assignment.
         /// </summary>
         public string assigmentName { get; set; }
 
         /// <summary>
-        /// 
+        /// The description of the assignment or a part of it.
         /// </summary>
         public string partsDescription { get; set; }
 
         /// <summary>
-        /// 
+        /// The due date of an assignment.
         /// </summary>
         public DateTime assignmentDueDate { get; set; }
 
         /// <summary>
-        /// 
+        /// The date of submission.
         /// </summary>
         public DateTime submissionDate { get; set; }
 
         /// <summary>
-        /// 
+        /// The time of submission.
         /// </summary>
         public DateTime submissionTime { get; set; }
     }
