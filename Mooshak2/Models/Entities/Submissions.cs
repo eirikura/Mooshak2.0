a8de199d4fee80f0e3f990a580f1867ee2a6a29c
Hreiðar Ólafur Arnarsson <hreidara14@ru.is>
2016-05-12 22:05:19 +0000

    Bætti við accepted og resultData í Submissions entity. Bætti líka við nullable merkingum.

--- Mooshak2/Models/Entities/Submissions.cs
+++ Mooshak2/Models/Entities/Submissions.cs
@@ -27,17 +27,27 @@ namespace Mooshak2.Models.Entities
         /// <summary>
         /// An ID number of a programming language used in the submission.
         /// </summary>
-        public int languageID { get; set; }
+        public int? languageID { get; set; }
 
         /// <summary>
         /// An ID number of an review used to connect it to a submission.
         /// </summary>
-        public int reviewID { get; set; }
+        public int? reviewID { get; set; }
+
+        /// <summary>
+        /// Keeps track of whether a submission was correct or not. Can be null.
+        /// </summary>
+        public bool? accepted { get; set; }
 
         /// <summary>
         /// The url location of the file the student submitted.
         /// </summary>
         public string file { get; set; }
+
+        /// <summary>
+        /// The results for the submission. What tests were made and results for each. Error messages if any.
+        /// </summary>
+        public string resultData { get; set; }
 
         /// <summary>
         /// The date at which a part of an assignment was submitted.
