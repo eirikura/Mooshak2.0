b9a9a75306c02af93184ad939c61d0ba680425a9
Hreiðar Ólafur Arnarsson <hreidara14@ru.is>
2016-05-12 23:45:59 +0000

    Bætti við nýjum ViewModels fyrir Courses.

---
+++ Mooshak2/Models/ViewModel/CourseEditViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mooshak2.Models.ViewModel
+{
+    public class CourseEditViewModel
+    {
+        /// <summary>
+        /// The ID number of a course.
+        /// </summary>
+        public int courseID { get; set; }
+
+        /// <summary>
+        /// The name of a course.
+        /// </summary>
+        [Display(Name = "Course name")]
+        [Required(ErrorMessage = "Course name is required")]
+        public string courseName { get; set; }
+
+        /// <summary>
+        /// The description of a course.
+        /// </summary>
+        [Display(Description = "Course description")]
+        public string courseDescription { get; set; }
+    }
+}
---
+++ Mooshak2/Models/ViewModel/CourseViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Mooshak2.Models.ViewModel
+{
+    public class CourseViewModel
+    {
+        /// <summary>
+        /// The ID number of a course.
+        /// </summary>
+        public int courseID { get; set; }
+
+        /// <summary>
+        /// The name of a course.
+        /// </summary>
+        public string courseName { get; set; }
+
+        /// <summary>
+        /// The description of a course.
+        /// </summary>
+        public string courseDescription { get; set; }
+    }
+}
