6ec434e13f08fe312a43bad459795ef5ce74101c
Hreiðar Ólafur Arnarsson <hreidara14@ru.is>
2016-05-12 20:31:37 +0000

    Finished get and add functions for courses

--- Mooshak2/Services/CoursesService.cs
+++ Mooshak2/Services/CoursesService.cs
@@ -30,53 +30,113 @@ namespace Mooshak2.Services
         /// <returns>A course is returned with id, name and description.</returns>
         public Courses getCourseByCourseID(int courseID)
         {
-            //TODO:
-            //fetch course object variables
-            //return object
-            return null;
+            return db.Courses.SingleOrDefault(x => x.courseID == courseID);
         }
 
         /// <summary>
         /// This function returns a list of courses that a user is part of.
         /// </summary>
-        /// <param name="userID"></param>
-        /// <returns></returns>
+        /// <param name="userID">Takes in a user's ID number.</param>
+        /// <returns>A list of courses.</returns>
         public ICollection<Courses> getCoursesByUser(int userID)
         {
-            //TODO:
-            //pagination for long lists
-            return null;
+            var userCoursesQuery = (from userCourses in db.UsersAndCourses
+                                    where userCourses.userID == userID
+                                    join courses in db.Courses 
+                                    on userCourses.courseID equals courses.courseID
+                                    select courses).ToList();
+
+            return userCoursesQuery;
         }
 
         /// <summary>
         /// This function returns a list of all courses.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A list of courses.</returns>
         public ICollection<Courses> getAllCourses()
         {
-            //TODO:
-            //pagination for long lists
-            return null;
+            var courses = (from course in db.Courses
+                          select course).ToList();
+
+            return courses;
         }
 
         /// <summary>
         /// This function adds a new course to the database.
         /// </summary>
         /// <returns>Returns true if able to add, else returns false.</returns>
-        public bool addNewCourse()
+        public bool addNewCourse(Courses newCourse)
         {
-            //TODO:
-            return true;
+            bool successfullyAdded = false;
+
+            db.Courses.Add(newCourse);
+
+            try
+            {
+                db.SaveChanges();
+                successfullyAdded = true;
+            }
+            catch
+            {
+                successfullyAdded = false;
+            }
+
+            return successfullyAdded;
         }
 
         /// <summary>
         /// This function adds a new connection between a user and a course to the database.
         /// </summary>
         /// <returns>Returns true if able to add, else returns false.</returns>
-        public bool addNewCourseAndUserConnection()
+        public bool addNewCourseAndUserConnection(UsersAndCourses connectionToAdd)
         {
-            //TODO:
-            return true;
+            bool successfullyAdded = false;
+
+            db.UsersAndCourses.Add(connectionToAdd);
+
+            try
+            {
+                db.SaveChanges();
+                successfullyAdded = true;
+            }
+            catch
+            {
+                successfullyAdded = false;
+            }
+
+            return successfullyAdded;
+        }
+
+        /// <summary>
+        /// This function updates values for a chosen course when an admin changes its values.
+        /// Possible values to change are name and description.
+        /// </summary>
+        /// <param name="courseToChange">Takes in a course to change.</param>
+        /// <returns>Returns true if editing the course finished successfully, else false.</returns>
+        public bool updateCourseInfo (Courses courseToChange)
+        {
+            bool successfullyEdited = false;
+
+            try
+            {
+                var query = (from course in db.Courses
+                             where course.courseID == courseToChange.courseID
+                             select course).SingleOrDefault();
+
+                if (query != null)
+                {
+                    query.name = courseToChange.name;
+                    query.description = courseToChange.description;
+                    db.SaveChanges();
+                    successfullyEdited = true;
+                }
+            }
+            catch
+            {
+                successfullyEdited = false;
+            }
+
+            return successfullyEdited;
         }
     }
 }
