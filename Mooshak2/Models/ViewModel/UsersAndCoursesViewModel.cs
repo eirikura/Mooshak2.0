fa1bb3a28135cfc93b17a1cc7f1adccc4b292f0a
Hreiðar Ólafur Arnarsson <hreidara14@ru.is>
2016-05-13 01:27:57 +0000

    Bætti við ViewModel-i fyrir UsersAndCourses tenginguna.

---
+++ Mooshak2/Models/ViewModel/UsersAndCoursesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.ViewModel
+{
+    public class UsersAndCoursesViewModel
+    {
+        /// <summary>
+        /// The user ID number used to connect to a course ID number.
+        /// </summary>
+        public int userID { get; set; }
+
+        /// <summary>
+        /// The course ID number used to connect to a user ID number.
+        /// </summary>
+        public int courseID { get; set; }
+    }
+}
