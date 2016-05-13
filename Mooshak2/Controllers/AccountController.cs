93c3fc6691e95cf2cd6a29d107e9443127459ea9
Maciuska <Maciuska14@gmail.com>
2016-05-13 17:55:33 +0000

    Fixing already working features

--- Mooshak2/Controllers/AccountController.cs
+++ Mooshak2/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace Mooshak2.Controllers
             {
                 int ID = userID.Value;
 
-                var viewModel = _service.getUserByID(ID);
+                var viewModel = _service.getUserByEmail(ID);
                 return View(viewModel);
             }
             return View();
